47976c49a0b795e4793c9afd5e5e636356b85171
Gabe Smith <gabe@paperlesspost.com>
2012-11-21 00:16:28 -0500

    clean up field_lists

--- ComicVine.py
+++ ComicVine.py
@@ -15,13 +15,15 @@ def debug(message):
     print '%s: %s' % (parent, message)
 
 def __get_json(method, data):
+  request_url = '%s/%s' % (__cv_api_url, method)
+  debug('%s, params: %s' % (request_url, data))
+
   data = dict({
     'api_key': __cv_api_key,
     'format': 'json'
   }, **data)
-  request_url = '%s/%s' % (__cv_api_url, method)
-  debug('%s, params: %s' % (request_url, data))
   response = requests.get(request_url, params=data)
+  debug(response.url)
 
   json = response.json
   if json['status_code'] == 1:
@@ -30,45 +32,36 @@ def __get_json(method, data):
     debug('ComicVine Error %(status_code)s: %(error)s' % json)
     return False
 
+series_fields = ','.join(['id', 'name', 'publisher', 'start_year', 'count_of_issues', 'issues'])
+issue_fields = ','.join(['id', 'name', 'volume', 'issue_number', 'publish_year', 'publish_month', 'publish_day'])
+
 def __search(query, data):
   """ http://api.comicvine.com/documentation/#search """
   data = dict({'query': query}, **data)
-  if 'field_list' in data:
-    data['field_list'] = ','.join(data['field_list'])
   return __get_json('search', data)
 
 def search_series(query):
   data = {
     'resources': 'volume',
-    'field_list': [
-      'id',
-      'name',
-      'publisher',
-      'start_year',
-      'count_of_issues'
-    ]
+    'field_list': series_fields
   }
   return __search(query, data)
 
 def search_issues(query):
   data = {
     'resources': 'issue',
-    'field_list': [
-      'id',
-      'name',
-      'volume',
-      'issue_number',
-      'publish_year',
-      'publish_month',
-      'publish_day'
-    ]
+    'field_list': issue_fields
   }
   return __search(query, data)
 
-def __view_object(obj_type, obj_id, data):
+def __get_object(obj_type, obj_id, data):
   method = '%s/%s/' % (obj_type, obj_id)
   return __get_json(method, data)
-def view_series(id):
-  return __view_object('volume', id, {})
-def view_issue(id):
-  return __view_object('issue', id, {})
+
+def get_series(obj_id):
+  data = {'field_list': series_fields}
+  return __get_object('volume', obj_id, data)
+
+def get_issue(obj_id):
+  data = {'field_list': issue_fields}
+  return __get_object('issue', obj_id, data)
--- vinery.py
+++ vinery.py
@@ -13,5 +13,8 @@ ComicVine.set_debug(True)
 # label.show()
 
 # sys.exit( app.exec_() )
-results = ComicVine.search_issues('Walking Dead')
+results = ComicVine.search_series('Walking Dead')
+# results = ComicVine.search_issues('Walking Dead')
+# results = ComicVine.get_series(18166) # Walking Dead
+# results = ComicVine.get_issue(115705) # Walking Dead #3
 print json.dumps(results, indent=4)
