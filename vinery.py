f038c500c151c52bc648784425794ef2b0f214ab
Gabe Smith <gabe@paperlesspost.com>
2012-11-24 17:48:14 -0500

    custom QTreeWidget and QTreeWidgetItem

--- vinery.py
+++ vinery.py
@@ -6,24 +6,36 @@ from PySide.QtCore import Qt
 
 ComicVine.set_debug(True)
 
+class SearchTree(QtGui.QTreeWidget):
+  def __init__(self, parent=None):
+    super(SearchTree, self).__init__(parent)
+    self.header().setVisible(False)
+    self.setWindowTitle("Search")
+    self.itemExpanded.connect(self.showSeries)
+
+  def showSeries(self, item, column=0):
+    data = item.data(1, Qt.UserRole)
+    print data['id']
+
+class SeriesSearchItem(QtGui.QTreeWidgetItem):
+  def __init__(self, data):
+    super(SeriesSearchItem, self).__init__()
+    label = '%s (%s) [%s]' % (res['name'], res['start_year'], res['publisher']['name'])
+    self.setText(0, label)
+    self.setData(1, Qt.UserRole, data)
+    self.addChild( self.loadingItem() )
+
+  def loadingItem(self):
+    item = QtGui.QTreeWidgetItem()
+    item.setText(0, "Loading...")
+    return item
+
 # Create the application object
 app = QtGui.QApplication(sys.argv)
-
-tree = QtGui.QTreeWidget()
-tree.setWindowTitle("Simple Tree Model")
-tree.header().setVisible(False)
-
 search = ComicVine.Series.search('Walking Dead')
 
-items = []
-for res in search['results']:
-  label = '%s (%s) [%s]' % (res['name'], res['start_year'], res['publisher']['name'])
-
-  item = QtGui.QTreeWidgetItem()
-  item.setText(0, label)
-  item.setData(1, Qt.UserRole, res)
-  items.append(item)
-
+tree = SearchTree()
+items = [SeriesSearchItem(res) for res in search['results']]
 tree.insertTopLevelItems(0, items)
 tree.show()
 
