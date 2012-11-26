f457dfb13f027d14954e008a05fd34d08f85891b
Gabe Smith <gabe@paperlesspost.com>
2012-11-25 21:40:22 -0500

    show series details instead of requesting issues

--- vinery.py
+++ vinery.py
@@ -11,42 +11,24 @@ class SearchTree(QtGui.QTreeWidget):
     super(SearchTree, self).__init__(parent)
     self.header().setVisible(False)
     self.setWindowTitle("Search")
-    self.itemExpanded.connect(self.showSeries)
 
   def loadSearch(self, query):
     search = ComicVine.Series.search(query)
     items = [SeriesSearchItem(result) for result in search['results']]
     self.insertTopLevelItems(0, items)
 
-  def showSeries(self, item, column=0):
-    if not item.loadedIssues:
-      series_id = item.json()['id']
-      search = ComicVine.Series.show(series_id)
-      issues = [IssueSearchItem(issue) for issue in search['results']['issues']]
-      item.takeChildren()
-      item.addChildren(issues)
-      item.loadedIssues = True
-
 class SeriesSearchItem(QtGui.QTreeWidgetItem):
   def __init__(self, data):
     super(SeriesSearchItem, self).__init__()
-    label = '%s (%s) [%s]' % (data['name'], data['start_year'], data['publisher']['name'])
+    label = '%s (%s)' % (data['name'], data['start_year'])
     self.setText(0, label)
     self.setData(1, Qt.UserRole, data)
-    self.addChild(QtGui.QTreeWidgetItem())
-    self.loadedIssues = False
 
-  def json(self):
-    return self.data(1, Qt.UserRole)
-
-class IssueSearchItem(QtGui.QTreeWidgetItem):
-  def __init__(self, data):
-    super(IssueSearchItem, self).__init__()
-    label = '#%s' % data['issue_number'].strip().rstrip("0").rstrip('.')
-    if data['name'].strip():
-      label += ' - %s' % data['name'].strip()
-    self.setText(0, label)
-    self.setData(1, Qt.UserRole, data)
+    publisher = QtGui.QTreeWidgetItem()
+    publisher.setText(0, 'Publisher: %s' % data['publisher']['name'])
+    issues = QtGui.QTreeWidgetItem()
+    issues.setText(0, '%s issues' % data['count_of_issues'])
+    self.addChildren([publisher, issues])
 
   def json(self):
     return self.data(1, Qt.UserRole)
@@ -62,23 +44,21 @@ class MainWindow(QtGui.QWidget):
   def doSearch(self):
     query = self.searchBox.text()
     if query:
-      self.tree.loadSearch(query)
-
+      self.searchResults.loadSearch(query)
 
   def searchGroup(self):
-    group = QtGui.QGroupBox('')
-    label = QtGui.QLabel("Search:")
-
     self.searchBox = QtGui.QLineEdit()
     self.searchBox.returnPressed.connect(self.doSearch)
     self.searchBox.setFocus()
 
-    self.tree = SearchTree()
+    self.searchResults = SearchTree()
 
     layout = QtGui.QGridLayout()
-    layout.addWidget(label, 0, 0)
+    layout.addWidget(QtGui.QLabel("Search:"), 0, 0)
     layout.addWidget(self.searchBox, 0, 1)
-    layout.addWidget(self.tree, 1, 0, 2, 2)
+    layout.addWidget(self.searchResults, 1, 0, 2, 2)
+
+    group = QtGui.QGroupBox('')
     group.setLayout(layout)
     return group
 
@@ -86,11 +66,6 @@ class MainWindow(QtGui.QWidget):
 app = QtGui.QApplication(sys.argv)
 window = MainWindow()
 window.show()
-
-#tree = SearchTree()
-#tree.loadSearch("Walking Dead")
-#tree.show()
-
 sys.exit( app.exec_() )
 
 
